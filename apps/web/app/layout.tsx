f65c46e107260e36e6b2aed6be070df6560f44da
busyhe <busyhe@qq.com>
2025-06-05 19:55:15 +0800

    feat: add metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,57 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-
+import { Metadata, Viewport } from 'next'
 import '@workspace/ui/globals.css'
 import { Providers } from '@/components/providers'
+import { fontSans, fontMono } from '@/lib/fonts'
+import { META_THEME_COLORS, siteConfig } from '@/config/site'
 
-const fontSans = Geist({
-  subsets: ['latin'],
-  variable: '--font-sans'
-})
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ['Next.js', 'React'],
+  authors: [
+    {
+      name: 'busyhe',
+      url: 'https://github.com/busyhe'
+    }
+  ],
+  creator: 'busyhe',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@busyhe'
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
+}
 
-const fontMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-mono'
-})
+export const viewport: Viewport = {
+  themeColor: META_THEME_COLORS.light
+}
 
 export default function RootLayout({
   children
