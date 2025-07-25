7c7040e28449288d1a3b920b1f891e13ad8dffa4
busyhe <busyhe@qq.com>
2025-07-25 14:42:13 +0800

    feat: add ogImage

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,7 +1,8 @@
 export const siteConfig = {
   name: 'Next.js Starter',
   url: 'https://github.com/busyhe/nextjs-starter',
-  ogImage: 'https://ui.shadcn.com/og.jpg',
+  ogImage:
+    'https://og-image-craigary.vercel.app/**Next.js%20Starter**.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg',
   description: 'A starter template for Next.js 15 with Shadcn UI and Tailwind CSS',
   links: {
     homepage: 'https://busyhe.com',
