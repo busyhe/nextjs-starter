96c321794f2704bfb4ba56479df5e67fd0c60aa0
busyhe <busyhe@qq.com>
2026-01-14 14:15:20 +0800

    feat: remove site header logo hidden

--- apps/web/components/header/main-nav.tsx
+++ apps/web/components/header/main-nav.tsx
@@ -6,7 +6,7 @@ import { Logo } from '@/components/logo'
 
 export function MainNav() {
   return (
-    <div className="mr-4 hidden md:flex">
+    <div className="mr-4 md:flex">
       <Link href="/" className="mr-4 flex items-center gap-2 lg:mr-6">
         <Logo className="size-6 rounded-sm" />
         <span className="hidden font-bold lg:inline-block">{siteConfig.name}</span>
