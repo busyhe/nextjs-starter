70f162d53c09de702d46d06bd779da7cd822aa5d
busyhe <busyhe@qq.com>
2026-03-05 11:29:56 +0800

    test: add docker build

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   transpilePackages: ["@workspace/ui"],
   env: {
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
